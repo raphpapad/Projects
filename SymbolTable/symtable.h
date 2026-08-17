104bf5763777f0f4cdc8e85d3da1342357fc12e5
Rafail Papadakis <papadakisraphael@gmail.com>
2026-08-17 15:45:20 +0300

    Symbol Table

--- SymbolTable/symtable.h
+++ SymbolTable/symtable.h
@@ -14,6 +14,7 @@ int SymTable_put(SymTable_T oSymTable, const char *pcKey, const void *pvValue);
 int SymTable_remove(SymTable_T oSymTable, const char *pcKey);
 
 int SymTable_contains(SymTable_T oSymTable, const char *pcKey);
+
 SymTable_T Symtable_put(SymTable_T oSymTable, const char *pcKey, const void *pvValue);
 
 void *SymTable_get(SymTable_T oSymTable, const char *pcKey);
